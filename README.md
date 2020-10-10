49d26f9d3f78e14bc3652c8aac6b0fb21a311016
Simon <wsj_simon@163.com>
2020-10-10 09:23:41 +0800

    add DetectCycle

--- README.md
+++ README.md
@@ -87,7 +87,7 @@
 ### medium
 
 - [109. 有序链表转换二叉搜索树](https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree/)
-- ...
+- [142. 环形链表 II](https://leetcode-cn.com/problems/linked-list-cycle-ii/) == REDO
 
 ### hard
 
---
+++ medium/linkedList/DetectCycle.java
@@ -0,0 +1,43 @@
+// 142. 环形链表 II
+// https://leetcode-cn.com/problems/linked-list-cycle-ii/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+  public ListNode detectCycle(ListNode head) {
+    if (head == null) {
+      return null;
+    }
+    ListNode slow = head;
+    ListNode fast = head;
+
+    while (fast != null) {
+      slow = slow.next;
+
+      if (fast.next != null) {
+        fast = fast.next.next;
+      } else {
+        return null;
+      }
+
+      if (fast == slow) {
+        ListNode ptr = head;
+
+        while (ptr != slow) {
+          ptr = ptr.next;
+          slow = slow.next;
+        }
+        return ptr;
+      }
+    }
+    return null;
+  }
+}
