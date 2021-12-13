17748830d89106f46c3bec27a87596caf99fd458
Simon <wsj_simon@163.com>
2021-12-13 10:41:51 +0800

    add treeToDoublyList

---
+++ medium/backtracking/offer-treeToDoublyList.cpp
@@ -0,0 +1,57 @@
+// 剑指 Offer 36. 二叉搜索树与双向链表
+// https://leetcode-cn.com/problems/er-cha-sou-suo-shu-yu-shuang-xiang-lian-biao-lcof/
+/*
+// Definition for a Node.
+
+*/
+// class Node {
+// public:
+//     int val;
+//     Node* left;
+//     Node* right;
+
+//     Node() {}
+
+//     Node(int _val) {
+//         val = _val;
+//         left = NULL;
+//         right = NULL;
+//     }
+
+//     Node(int _val, Node* _left, Node* _right) {
+//         val = _val;
+//         left = _left;
+//         right = _right;
+//     }
+// };
+
+
+class Solution {
+public:
+  Node* treeToDoublyList(Node* root) {
+    if (root == nullptr) {
+      return nullptr;
+    }
+    dfs(root);
+    head->left = pre;
+    pre->right = head;
+    return head;
+  }
+
+private:
+  Node *pre, *head;
+  void dfs (Node* cur) {
+    if (cur == nullptr) {
+      return;
+    }
+    dfs(cur->left);
+    if (pre != nullptr) {
+      pre->right = cur;
+    } else {
+      head = cur;
+    }
+    cur->left = pre;
+    pre = cur;
+    dfs(cur->right);
+  }
+};
